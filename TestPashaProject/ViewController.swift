caec195d8d4a7b7063aefabf797293af2938144f
Moroz Pavel <vferme2015@gmail.com>
2020-06-20 15:02:10 +0300

    Initial Commit

---
+++ TestPashaProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPashaProject
+//
+//  Created by Pavel Moroz on 20.06.2020.
+//  Copyright © 2020 Pavel Moroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
